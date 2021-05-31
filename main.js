f63ed18c1e78d74b74f6e7ef918d7d310fdaa1dd
ddu6 <ddu6@protonmail.com>
2021-05-31 20:12:44 +0800

    update

---
+++ main.js
@@ -0,0 +1,25 @@
+export function card(unit){
+    let {name,desc,src,href}=unit.options.object
+    const element=document.createElement('a')
+    const nameEle=document.createElement('div')
+    const descEle=document.createElement('div')
+    if(typeof name==='string')nameEle.textContent=name
+    if(typeof desc==='string')descEle.textContent=desc
+    if(typeof src==='string'){
+        element.href='?src='+encodeURIComponent(src)
+    }else if(typeof href==='string'){
+        if(href.endsWith('.pdf')){
+            element.href='https://mozilla.github.io/pdf.js/web/viewer.html?file='+encodeURIComponent(href)
+        }else{
+            element.href=href
+        }
+    }
+    element.target='_blank'
+    element.classList.add('card')
+    element.classList.add('pre')
+    nameEle.classList.add('name')
+    descEle.classList.add('desc')
+    element.append(nameEle)
+    element.append(descEle)
+    return element
+}
---
+++ st/main.js
@@ -0,0 +1,10 @@
+const script=document.createElement('script')
+script.src='https://cdn.jsdelivr.net/gh/ddu6/st@0.1.0/dist/editor.js'
+const current=document.currentScript
+if(current instanceof HTMLScriptElement){
+    const src=current.dataset.src
+    if(typeof src==='string'&&src.length>0){
+        script.dataset.src=src
+    }
+}
+document.body.append(script)
