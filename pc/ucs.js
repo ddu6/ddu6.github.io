5521a2d4439811975224cc063b2badb2e925a757
ddu <ddu6@protonmail.com>
2021-07-18 02:03:54 +0800

    update

--- pc/ucs.js
+++
@@ -1,7 +0,0 @@
-export function va(unit){
-    const a=document.createElement('a')
-    a.target='_blank'
-    a.href=`https://ddu6.github.io/player/?src=${encodeURIComponent(`https://github.com/pku6/${unit.options['course-id']}/releases/download/v0.0.1/${unit.options.name}.mp4`)}`
-    a.textContent=unit.options.name
-    return a
-}
