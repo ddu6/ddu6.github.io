cf40cadac904f4a69a617f12464516242b77c19c
DDU <ddu6@protonmail.com>
2022-02-05 03:34:34 +0800

    update

--- easthouse/main.js
+++ easthouse/main.js
@@ -2,3 +2,12 @@ addEventListener('wheel', e => {
     e.preventDefault()
     scrollBy(e.deltaX + e.deltaY, 0)
 }, {passive: false})
+const consult = document.querySelector('#consult')
+const form = document.querySelector('form')
+consult.addEventListener('click', e => {
+    e.stopPropagation()
+    form.style.display = 'flex'
+})
+addEventListener('click', () => {
+    form.style.display = 'none'
+})
