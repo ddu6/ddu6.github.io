16669e56114f4fc8753adf76082ef21c8c2d7aed
DDU <ddu6@protonmail.com>
2022-02-05 00:49:56 +0800

    update

--- easthouse/main.js
+++ easthouse/main.js
@@ -1,5 +1,4 @@
-const main = document.body.children[1]
-main.addEventListener('wheel', e => {
+addEventListener('wheel', e => {
     e.preventDefault()
-    main.scrollBy(e.deltaX + e.deltaY, 0)
+    scrollBy(e.deltaX + e.deltaY, 0)
 }, {passive: false})
