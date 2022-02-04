422d9132bae769a1001996a554e16f542360510b
DDU <ddu6@protonmail.com>
2022-02-05 00:33:29 +0800

    update

--- easthouse/main.js
+++ easthouse/main.js
@@ -2,3 +2,6 @@ addEventListener('wheel', e => {
     e.preventDefault()
     scrollBy(e.deltaX + e.deltaY, 0)
 }, {passive: false})
+setTimeout(() => {
+    document.body.style.display = 'block'
+})
