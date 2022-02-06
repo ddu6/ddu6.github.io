7be6bcd9e1f621072be15633ee2b387c4874f8ca
DDU <ddu6@protonmail.com>
2022-02-06 19:48:01 +0800

    update

--- easthouse/main.js
+++ easthouse/main.js
@@ -3,11 +3,21 @@ const form = document.querySelector('form')
 const main = document.querySelector('main')
 const rect = document.querySelector('#history-rect')
 const footer = document.querySelector('footer')
-main.addEventListener('wheel', e => {
-    e.preventDefault()
-    const delta = e.deltaX + e.deltaY
-    main.scrollBy(delta, 0)
-}, {passive: false})
+for (let i = 0; i < main.children.length; i++) {
+    const part = main.children[i]
+    const tab = footer.children[i]
+    const line = document.createElement('div')
+    tab.prepend(line)
+    line.style.height = '1px'
+    line.style.marginBottom = '.5em'
+    part.addEventListener('wheel', e => {
+        e.preventDefault()
+        const delta = e.deltaX + e.deltaY
+        part.scrollBy(delta, 0)
+    }, {passive: false})
+    part.addEventListener('scroll', update)
+    part.addEventListener('touchmove', update)
+}
 consult.addEventListener('click', e => {
     e.stopPropagation()
     form.style.display = 'flex'
@@ -25,33 +35,29 @@ addEventListener('click', e => {
         }
         e.preventDefault()
         if (href.length === 1) {
-            main.children[0].scrollIntoView({behavior: 'smooth', inline: 'start'})
+            const part = main.children[0]
+            part.scrollLeft = 0
+            part.scrollIntoView({behavior: 'smooth', inline: 'start'})
             break
         }
         const id = decodeURIComponent(href.slice(1))
         const result = document.body.querySelector(`[id=${JSON.stringify(id)}]`)
         if (result !== null) {
+            result.scrollLeft = 0
             result.scrollIntoView({behavior: 'smooth', inline: 'start'})
         }
         break
     }
 })
-for (let i = 0; i < footer.children.length; i++) {
-    const tab = footer.children[i]
-    const line = document.createElement('div')
-    tab.prepend(line)
-    line.style.height = '1px'
-    line.style.marginBottom = '.5em'
-}
 function update() {
     for (let i = 0; i < main.children.length; i++) {
         const part = main.children[i]
         const tab = footer.children[i]
         const line = tab.children[0]
-        const {left, right, width} = part.getBoundingClientRect()
+        const {left, right} = part.getBoundingClientRect()
         if (left <= visualViewport.width / 2 && right >= visualViewport.width / 2) {
-            const percent = 100 * (visualViewport.width - left) / width
-            const rightPercent = Math.max(0, -100 * left / (width - visualViewport.width))
+            const percent = 100 * (visualViewport.width - part.scrollLeft) / part.scrollWidth
+            const rightPercent = Math.max(0, -100 * part.scrollLeft / (part.scrollWidth - visualViewport.width))
             part.classList.remove('fade')
             tab.style.opacity = '1'
             line.style.background = `linear-gradient(to right, goldenrod ${percent}%, lightgray ${percent}%)`
@@ -73,5 +79,8 @@ function update() {
 setTimeout(() => {
     update()
 }, 100)
+setTimeout(() => {
+    update()
+}, 1000)
 main.addEventListener('scroll', update)
 main.addEventListener('touchmove', update)
